c279f2b57d05807aa0c02884153a44d84541d16a
hope.com <694994478@qq.com>
2017-05-08 13:25:46 +0800

    SpringMVC+myBatis+redis集群配置

---
+++ src/main/java/com/ych/ssm/service/impl/UserServiceImpl.java
@@ -0,0 +1,45 @@
+package com.ych.ssm.service.impl;
+
+//import RedisCache;
+import com.ych.ssm.service.UserService;
+import com.ych.ssm.cache.RedisClusterCache;
+import com.ych.ssm.dao.UserDao;
+import com.ych.ssm.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	private UserDao userDao;
+//	@Autowired
+//	private RedisCache cache;
+	@Autowired
+	private RedisClusterCache cache;
+	
+	
+	@Override
+	public List<User> getUserList(int offset, int limit) {
+		String cache_key=RedisClusterCache.CAHCENAME+"|getUserList|"+offset+"|"+limit;
+		//先去缓存中取
+		List<User> result_cache=cache.getListCache(cache_key, User.class);
+		if(result_cache==null){
+			//缓存中没有再去数据库取，并插入缓存（缓存时间为60秒）
+			result_cache=userDao.queryAll(offset, limit);
+			cache.putListCacheWithExpireTime(cache_key, result_cache, RedisClusterCache.CAHCETIME);
+			LOG.info("put cache with key:"+cache_key);
+		}else{
+			LOG.info("get cache with key:"+cache_key);
+		}
+		return result_cache;
+	}
+	
+	
+
+}
